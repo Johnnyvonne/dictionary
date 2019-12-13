4dda1f5fe0913546103597e5ae12e76293038686
zanglei <0620isabella@163.com>
2019-12-13 16:40:51 +0800

    first commit

---
+++ src/test/java/nature/wildcode/dictionary/DictionaryApplicationTests.java
@@ -0,0 +1,13 @@
+package nature.wildcode.dictionary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DictionaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
